840395362a11b7926e59f20c61ee79c3df1f8225
angelynafaa <angelyna826@gmail.com>
2020-06-14 14:33:41 +0700

    memuat library js kedalam service worker

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+ 
+if (workbox)
+  console.log(`Workbox berhasil dimuat`);
+else
+  console.log(`Workbox gagal dimuat`);
