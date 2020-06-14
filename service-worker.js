531160a52da9bea2cf8205f31a5e2adf6e85fa84
angelynafaa <angelyna826@gmail.com>
2020-06-14 14:45:19 +0700

    untuk menyimpan semua berkas halaman yang ada di folder pages

--- service-worker.js
+++ service-worker.js
@@ -4,3 +4,16 @@ if (workbox)
   console.log(`Workbox berhasil dimuat`);
 else
   console.log(`Workbox gagal dimuat`);
+//pastikan kalo ngubah yang sudah di daftarain dibawah revison nya diganti biar ke update
+  workbox.precaching.precacheAndRoute([
+      { url: `/index.html`, revision: '1'}, 
+      { url: '/nav.html', revision: '1' },
+      { url: '/css/materialize.min.css', revision: '1' },
+      { url: '/js/materialize.min.js', revision: '1' },
+      { url: '/js/script.js', revision: '1' },
+  ]);
+
+  workbox.routing.registerRoute(
+    new RegExp('/pages/'),
+    workbox.strategies.staleWhileRevalidate()
+  );
